5e02e5c1ccc5f2eee948c045557b993b5a979644
Петрикевич Анастасия <nastya.petrikevich@gmail.com>
2018-03-18 17:54:31 +0300

    Add project files.

---
+++ FindNthRoot.Logic/Root.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindNthRoot.Logic
+{
+    /// <summary>
+    /// This class contains methods for finding the root of a number. 
+    /// </summary>
+    public static class Root
+    {
+        /// <summary>
+        /// Finds the Nth root of the number using the Newton algorithm.
+        /// </summary>
+        /// <param name="number">Your number.</param>
+        /// <param name="degree">Root degree.</param>
+        /// <param name="precision">Prescision of the calculation.</param>
+        /// <returns>Root value.</returns>
+
+        public static double FindNthRoot(double number, int degree, double precision)
+        {
+            if (number < 0 && (degree % 2 == 0))
+            {
+                throw new ArgumentOutOfRangeException(nameof(number));
+            }
+            if (degree < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(degree));
+            }
+
+            if (precision < 0 || precision > 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(number));
+            }
+
+
+            double first = number / degree;
+            double second = 1 / (double)degree * (((double)degree - 1) * first + number / Math.Pow(first, degree - 1));
+
+            while (Math.Abs(second - first) > precision)
+            {
+                first = second;
+                second = 1 / (double)degree * (((double)degree - 1) * first + number / Math.Pow(first, degree - 1));
+
+            }
+
+            return Math.Round(second, 3);
+        }
+    }
+}
---
+++ FindNthRoot.Tests/RootTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FindNthRoot.Logic;
+
+namespace FindNthRoot.Tests
+{
+    [TestFixture]
+    public class RootTests
+    {
+        [TestCase(1, 5, 0.0001, ExpectedResult = 1)]
+        [TestCase(8, 3, 0.0001, ExpectedResult = 2)]
+        [TestCase(0.001, 3, 0.0001, ExpectedResult = 0.1)]
+        [TestCase(0.04100625, 4, 0.0001, ExpectedResult = 0.45)]
+        [TestCase(8, 3, 0.0001, ExpectedResult = 2)]
+        [TestCase(0.0279936, 7, 0.0001, ExpectedResult = 0.6)]
+        [TestCase(0.0081, 4, 0.1, ExpectedResult = 0.3)]
+        [TestCase(-0.008, 3, 0.1, ExpectedResult = -0.2)]
+        [TestCase(0.004241979, 9, 0.00000001, ExpectedResult = 0.545)]
+        public static double FindNthRoot_Result(double number, int degree, double prescision) =>
+            Root.FindNthRoot(number, degree, prescision);
+
+        [Test]
+        public void FindNthRoot_Number_8_Degree_Minus3_Throw_ArgumentOutOfRangeException()
+            => Assert.Throws<ArgumentOutOfRangeException>(() => Root.FindNthRoot(8, -3, 0.001));
+        [Test]
+        public void FindNthRoot_Precision_4_Throw_ArgumentOutOfRangeException()
+            => Assert.Throws<ArgumentOutOfRangeException>(() => Root.FindNthRoot(8, 3, 4));
+        [Test]
+        public void FindNthRoot_Precision_Minus1_Throw_ArgumentOutOfRangeException()
+            => Assert.Throws<ArgumentOutOfRangeException>(() => Root.FindNthRoot(8, 3, -1));
+    }
+}
